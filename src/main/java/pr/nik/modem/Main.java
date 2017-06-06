361a61c5ea00dab5d22e434b76a8233eaee39008
naduda <naduda.pr@gmail.com>
2017-06-06 09:56:47 +0300

    Executors.newFixedThreadPool

--- src/main/java/pr/nik/modem/Main.java
+++ src/main/java/pr/nik/modem/Main.java
@@ -1,23 +1,21 @@
 package pr.nik.modem;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class Main {
-  public static final CommonFile cf = new CommonFile();
+  private static final int SOCKET_PORT = 7777;
+  private static int SOCKET_TIMEOUT = 30_000;
+  private static CommonFileSingleton cf;
   private static List<String> listIp;
   private static List<String> listCommands;
-  public static String successFile;
-  public static String errorIp;
   private static int THREADS_COUNT = 1;
-  public static int SOCKET_TIMEOUT = 30_000;
-  private static int runningCount = 0;
-
-  private SocketTools st;
 
   public static void main(String[] args) {
+    cf = CommonFileSingleton.getInstance();
+
     if (args.length < 1) {
       System.out.println(Help.TEXT);
       return;
@@ -30,75 +28,32 @@ public class Main {
     if (args.length > 2) {
       SOCKET_TIMEOUT = Integer.parseInt(args[2]) * 1000;
     }
-    
-    System.err.println(THREADS_COUNT + " === " + SOCKET_TIMEOUT + ": " + new Date());
 
     String fileName = args[0];
     String fileNameCmd = fileName.substring(0, fileName.indexOf(".")) + "_cmd.txt";
-    successFile = cf.createNewFile(fileName, "Success");
-    errorIp = cf.createNewFile(fileName, "errorIp");
+    String successFile = cf.createNewFile(fileName, "Success");
+    String errorIp = cf.createNewFile(fileName, "errorIp");
 
+    ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
     readFiles(fileName, fileNameCmd);
-    listIp.stream()
-      .map(f -> f.split("#")[0].trim())
-      .filter(f -> f.length() > 0)
-      .forEach(ip -> {
-        changeRoundCount(1);
-        new Thread(() -> {
-          Main instance = new Main();
-          String logFile = cf.createNewFile(fileName, "log_" + ip);
-          instance.st = new SocketTools(logFile);
-          instance.executeListOfCommands(ip, successFile, errorIp);
-          instance.st.close();
-          changeRoundCount(-1);
-        }).start();
-
-        while (runningCount == THREADS_COUNT) {
-          try {
-            Thread.sleep(1000);
-          } catch (InterruptedException e) {
-            System.err.println(e.getMessage());
-          }
-        }
+    listIp.forEach(ip -> {
+        String logFile = cf.createNewFile(fileName, "log_" + ip);
+        SimpleSocket simpleSocket = new SimpleSocket(ip, SOCKET_PORT, SOCKET_TIMEOUT, logFile, successFile, errorIp);
+        simpleSocket.setCommandsList(listCommands);
+        executor.submit(simpleSocket);
       });
-    System.err.println(THREADS_COUNT + " === " + SOCKET_TIMEOUT + ": " + new Date());
-  }
-  
-  private static synchronized void changeRoundCount(int value) {
-    runningCount += value;
-//    System.err.println(runningCount);
+    executor.shutdown();
   }
 
   private static void readFiles(String fileIp, String fileCmd) {
-    listIp = cf.getFileToListOfLines(fileIp);
-    listCommands  = cf.getFileToListOfLines(fileCmd);
-  }
-
-  private void executeListOfCommands(String ip, String successFile, String errorIp) {
-    List<String> cmdList = listCommands.stream()
+    listIp = cf.getFileToListOfLines(fileIp).stream()
         .map(f -> f.split("#")[0].trim())
-        .filter(f -> f.length() > 0).collect(Collectors.toList());
-    
-    boolean success = true;
-    for (int i = 0; i < cmdList.size(); i++) {
-      String cmdString = cmdList.get(i);
-      String cmd = cmdString.split("\\|")[0];
-      Map<String, String> params = cf.getCmdParameters(cmdString);
-      String response = st.executeCommand(cmd, ip, params);
-      if (response.equals("-1")) {
-        success = false;
-        break;
-      }
-      if (response.equals("-2")) {
-        break;
-      }
-    }
-    
-    if (success) {
-      cf.writeToFile(successFile, ip);
-    } else {
-      cf.writeToFile(errorIp, ip);
-    }
+        .filter(f -> f.length() > 0)
+        .collect(Collectors.toList());
+    listCommands  = cf.getFileToListOfLines(fileCmd).stream()
+        .map(f -> f.split("#")[0].trim())
+        .filter(f -> f.length() > 0)
+        .collect(Collectors.toList());
   }
 
 }
---
+++ src/main/java/pr/nik/modem/SimpleSocket.java
@@ -0,0 +1,190 @@
+package pr.nik.modem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+import java.util.Map;
+
+public class SimpleSocket implements Runnable {
+  private static CommonFileSingleton cf;
+  private Socket socket;
+  private PrintWriter out;
+  private BufferedReader in;
+  private String logFile;
+  private String successFile;
+  private String errorFile;
+  private String ipAddress;
+  private int port;
+  private int socketTimeout;
+  private List<String> listCommands;
+  
+  public SimpleSocket(String ipAddress, int port, int timeout, String logFile, String successFile, String errorFile) {
+    this.logFile = logFile;
+    this.errorFile = errorFile;
+    this.successFile = successFile;
+    this.ipAddress = ipAddress;
+    this.port = port;
+    this.socketTimeout = timeout;
+    
+    cf = CommonFileSingleton.getInstance();
+  }
+  
+  public boolean init() {
+    if (socket == null || socket.isClosed() || !socket.isConnected()) {
+      try {
+        socket = new Socket(ipAddress, port);
+        socket.setSoTimeout(socketTimeout);
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      } catch (IOException e) {
+        return false;
+      }
+      if (socket == null || socket.isClosed() || !socket.isConnected()) {
+        if (socket != null) {
+          try {
+            socket.close();
+            cf.writeToFile(logFile, "Socket closed!");
+          } catch (IOException e) {
+            cf.writeToFile(logFile, e.getMessage());
+          }
+        }
+      }
+      if (socket != null && socket.isConnected()) {
+        cf.writeToFile(logFile, String.format("Connected to %s!", ipAddress));
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  public void close() {
+    if (socket != null) {
+      try {
+        if (!socket.isClosed()) {
+          cf.writeToFile(logFile, "Socket closed!");
+        }
+        socket.close();
+      } catch (IOException e) {
+        cf.writeToFile(logFile, e.getMessage());
+      }
+    }
+    
+    if (out != null) {
+      out.close();
+    }
+    
+    if (in != null) {
+      try {
+        in.close();
+      } catch (IOException e) {
+        cf.writeToFile(logFile, e.getMessage());
+      }
+    }
+  }
+  
+  public boolean waitForConnect(long timeout) {
+    if (timeout == 0) {
+      timeout = 60_000;
+    }
+    long start = System.currentTimeMillis();
+    while (socket == null || socket.isClosed() || !socket.isConnected()) {
+      if (System.currentTimeMillis() - start > timeout) {
+        cf.writeToFile(logFile, "Didn't connect........................" + ipAddress);
+        break;
+      }
+      init();
+    }
+    return socket != null && !socket.isClosed() && socket.isConnected();
+  }
+
+  public CommandResult executeCommand(String cmd, Map<String, String> params) {
+    CommandResult result = CommandResult.ERROR;
+    String response = "";
+    long timeout = params.containsKey("timeout") ? Long.parseLong(params.get("timeout")) * 1000 : 0;
+
+    if (timeout == 0) {
+      if (!waitForConnect(socketTimeout * 3)) {
+        return result;
+      }
+    }
+
+    out.println(cmd);
+    cf.writeToFile(logFile, "=> " + cmd);
+    int dataBuffer;
+    StringBuilder respBuilder = new StringBuilder();
+    try {
+      while ((dataBuffer = in.read()) != -1) {
+        if (dataBuffer == 10) break;
+        respBuilder.append((char)dataBuffer);
+      }
+      response = respBuilder.toString();
+      cf.writeToFile(logFile, response);
+      if (params.containsKey("contains")) {
+        if (response.toLowerCase().contains(params.get("contains").toString())) {
+          result = CommandResult.SUCCESS;
+        }
+      }
+    } catch (IOException e) {
+      cf.writeToFile(logFile, e.getMessage());
+    }
+    if (params.containsKey("reconnect")) {
+      close();
+      result = CommandResult.SUCCESS;
+    }
+
+    if (params.containsKey("wait")) {
+      close();
+      try {
+        Thread.sleep(Long.parseLong(params.get("wait").toString()) * 1000);
+      } catch (InterruptedException e) {
+        cf.writeToFile(logFile, e.getMessage());
+      }
+      result = CommandResult.SUCCESS;
+    }
+
+    if (result == CommandResult.ERROR && params.containsKey("timeout")) {
+      close();
+      result = waitForConnect(timeout) ? CommandResult.SUCCESS : CommandResult.ERROR;
+    } else if (params.containsKey("exitifcontains")) {
+      boolean isSuccess = true;
+      String[] conditions = params.get("exitifcontains").toString().split("&&");
+      for (String condition : conditions) {
+        if (!isSuccess) break;
+        isSuccess = response.toLowerCase().contains(condition.trim());
+      }
+      if (isSuccess) {
+        return CommandResult.SUCCESS_AND_BREAK;
+      }
+    }
+
+    return result;
+  }
+  
+  public void setCommandsList(List<String> list) {
+    this.listCommands = list;
+  }
+
+  @Override
+  public void run() {
+    boolean success = true;
+    for (int i = 0; i < listCommands.size(); i++) {
+      String cmdString = listCommands.get(i);
+      String cmd = cmdString.split("\\|")[0];
+      Map<String, String> params = cf.getCmdParameters(cmdString);
+      CommandResult response = executeCommand(cmd, params);
+      success = response != CommandResult.ERROR;
+
+      if (response != CommandResult.SUCCESS) {
+        break;
+      }
+    }
+
+    cf.writeToFile(success ? successFile : errorFile, ipAddress);
+    close();
+  }
+}
